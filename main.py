c6c99458f0d03153a1b64aef202189f09060170a
Curtis Clemens <cwclemens@gmail.com>
2015-06-16 01:44:14 -0400

    Added command-line parsing.

--- main.py
+++ main.py
@@ -5,21 +5,34 @@ from camera import *
 from ray import *
 from vecmath import *
 from sceneparser import *
+import argparse
 import Image
 
 def main():
-  config = parseFile('bluesphere.scene')
-  array = np.zeros((200, 200, 3))
-  for i,x in enumerate(np.linspace(-1, 1, 200)):
-    for j,y in enumerate(np.linspace(1, -1, 200)):
+  cmd_args = getArguments()
+  xres, yres = cmd_args.res
+  config = parseFile(cmd_args.infile)
+  if isinstance(config.camera, PerspectiveCamera):
+    config.camera.aspect = yres/float(xres)
+  array = np.zeros((yres, xres, 3))
+  for i,x in enumerate(np.linspace(-1, 1, xres)):
+    for j,y in enumerate(np.linspace(1, -1, yres)):
       ray = config.camera.generateRay(vector(x,y))
       hit = config.scene.intersect(ray, 0)
       if hit == None:
-        array[j][i] = vector(0,0,0)
+        array[j][i] = config.background_color
       else:
         t, material, normal = hit
         array[j][i] = material.shade(t, ray, normal, config.lights[0])
-  write_image(array)
+  write_image(array, cmd_args.o)
+
+def getArguments():
+  """Use argparse to get the command-line arguments"""
+  parser = argparse.ArgumentParser(description='A simple raytracer')
+  parser.add_argument('infile', help='The input file path. The input file describes the scene to be raytraced.')
+  parser.add_argument('-o', help='The output image file path.')
+  parser.add_argument('-res', nargs=2, help='Output resolution.', metavar=('x','y'), type=int, default=[200,200])
+  return parser.parse_args()
 
 def write_image(array, outfile=None):
   """Use PIL to output NumPy array as image."""
